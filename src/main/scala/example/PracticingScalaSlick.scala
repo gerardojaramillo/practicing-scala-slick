5c681c8e0e2ac559b93f40819aaeff163e75be2d
Gerardo Jaramillo <millodev@Gerardos-MacBook-Pro.local>
2023-09-28 06:31:43 -0600

    practicing-scala-slick.

---
+++ src/main/scala/example/PracticingScalaSlick.scala
@@ -0,0 +1,12 @@
+/** PracticingScalaSlick.scala
+  */
+
+package example
+
+object PracticingScalaSlick {
+
+  def main(args: Array[String]): Unit = {
+    println("Practicing Scala Slick !")
+  }
+
+}
